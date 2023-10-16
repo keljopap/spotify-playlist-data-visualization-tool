7b8872600f13408ea82eb41fbd20c54010df6993
Kelli Pape <keljopap@gmail.com>
2023-10-16 17:29:25 -0400

    Handle multiple api permission scopes in one token

--- README.md
+++ README.md
@@ -8,19 +8,41 @@
 
 ## Feature Ideas
 
-- [ ] find playlists by a particular song
-- [ ] find repeat songs across different playlists
-- [ ] compare most similar playlists
+Playlist analysis:
+
 - [x] give playlists rankings based on danceability, tempo, metrics
+- [ ] actually paginate through more than the page limit of 50 and rank entire list (may hit api limits at certain playlist sizes, need to throttle)
+- [ ] compare most similar playlists
 - [ ] rank your friends' public playlists based on these too
 - [ ] combine any two playlists
 - [ ] identify "outlier" songs
 - [ ] identify most homogeneous and least homogeneous playlists
-- [ ] report listening trends over time
-- [ ] what time of day are you listening
-- [ ] rank a favorite artists' songs by audio features
+
+Playlist analysis visualization:
+
 - [ ] use a plugin to automatically do some of the data visualizations
 - [ ] create a session for an arbitrary user who authenticates and show them their data
+
+Playlist management:
+
+- [ ] find playlists by a particular song
+- [ ] find repeat songs across different playlists
+
+Artists' songs analysis:
+
+- [ ] rank a favorite artists' songs by audio features
+- [ ] identify outlier songs
+- [ ] compare homogeneity of two artists' songs across a certain category
+
+Stretch goals with account data zip  (these can only be done with account data zip - must request download per user instead of using api)
+
+- [ ] report listening trends over time
+- [ ] what time of day are you listening
+
+Recently listened to data:
+
+- [x] Use a different scope to fetch most recently listened to songs
+- [ ] Rate the "mood" of recent listens (perhaps against average moods of playlists)
 
 ## Sources and inspirations
 
--- src/main.py
+++ src/main.py
@@ -4,34 +4,45 @@ import json
 import rank_tracks_by_feature
 import user_inputs
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-library-read"))
+scopes = "user-library-read user-read-recently-played"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scopes))
 
-# 1. Ask the user how many tracks they'd like to list
-# 2. Ask the user which trait they want to sort by
+# TODO Ask the user how many tracks they'd like to list
+# TODO Ask the user which trait they want to sort by
 #selected_feature_key = user_inputs.select_feature_to_sort_by()
 SELECTED_FEATURE_KEY = "tempo"
-# 3. Ask the user if they want to sort in ascending or descending order
-#descending = user_inputs.sort_by_descending()
+# TODO Ask the user if they want to sort in ascending or descending order
+# descending = user_inputs.sort_by_descending()
 DESCENDING = True
 
-PLAYLIST_LIMIT=10
-TRACK_LIMIT=50
+# PLAYLIST_LIMIT=1
+TRACK_LIMIT=5
 
-saved_tracks = rank_tracks_by_feature.get_user_saved_tracks_with_audio_features(sp, TRACK_LIMIT)
-sorted = rank_tracks_by_feature.get_sorted_tracks_by_feature(saved_tracks, SELECTED_FEATURE_KEY, DESCENDING)
-rank_tracks_by_feature.print_list_of_tracks_sorted_by_feature(sorted, SELECTED_FEATURE_KEY)
+def print_saved_tracks_rankings():
+    saved_tracks = rank_tracks_by_feature.get_user_saved_tracks_with_audio_features(sp, TRACK_LIMIT)
+    sorted = rank_tracks_by_feature.get_sorted_tracks_by_feature(saved_tracks, SELECTED_FEATURE_KEY, DESCENDING)
+    rank_tracks_by_feature.print_list_of_tracks_sorted_by_feature(sorted, SELECTED_FEATURE_KEY)
 
-playlists_with_ratings = rank_tracks_by_feature.get_current_user_playlists_to_track_ids_with_audio_features(sp, PLAYLIST_LIMIT, TRACK_LIMIT)
-for pid in playlists_with_ratings:
-    playlist = playlists_with_ratings[pid]
-    print("\n\nFor playlist \"" + playlist['name'] + "\"")
-    new_sorted = rank_tracks_by_feature.get_sorted_tracks_by_feature(
-        playlist['tracks'],
-        SELECTED_FEATURE_KEY,
-        DESCENDING
-    )
-    rank_tracks_by_feature.print_list_of_tracks_sorted_by_feature(
-        new_sorted,
-        SELECTED_FEATURE_KEY,
-        DESCENDING
-    )
+def print_playlists_rankings():
+    playlists_with_ratings = rank_tracks_by_feature.get_current_user_playlists_to_track_ids_with_audio_features(sp, PLAYLIST_LIMIT, TRACK_LIMIT)
+    for pid in playlists_with_ratings:
+        playlist = playlists_with_ratings[pid]
+        print("\n\nFor playlist \"" + playlist['name'] + "\"")
+        new_sorted = rank_tracks_by_feature.get_sorted_tracks_by_feature(
+            playlist['tracks'],
+            SELECTED_FEATURE_KEY,
+            DESCENDING
+        )
+        rank_tracks_by_feature.print_list_of_tracks_sorted_by_feature(
+            new_sorted,
+            SELECTED_FEATURE_KEY,
+            DESCENDING
+        )
+
+print_saved_tracks_rankings()
+
+print("Most recently listened to songs...")
+recently_played_tracks = sp.current_user_recently_played()
+for idx, item in enumerate(recently_played_tracks['items'], start=1):
+    track = item['track']
+    print(idx, ".", track['id'], track['name'], "-", track['artists'][0]['name'], "-", track['album']['name'])
