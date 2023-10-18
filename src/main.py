963840266b8dcb091ae848fea4cb34130ab9e726
Kelli Pape <keljopap@gmail.com>
2023-10-18 16:40:10 -0400

    This renders playlists sorted by various features, though it does not handle songs of duplicate names or lazy loading the audio features quite yet. WIP WIP WIP

--- src/main.py
+++ src/main.py
@@ -1,13 +1,28 @@
 import defaults
 import genre_recs
+import json
 import playlists
 import recently_played
 import saved_tracks
 
-for sort_key in defaults.POSSIBLE_SORT_KEYS:
-    saved_tracks.print_saved_tracks_rankings(10, sort_key, True)
+#saved_tracks.print_saved_tracks_rankings(10, defaults.TRAIT_POPULARITY, True)
+#saved_tracks.print_saved_tracks_rankings(10, defaults.TRAIT_DANCEABILITY, True)
 
-playlists.print_playlists_rankings()
+#playlists.print_playlists_rankings()
 
-genre_recs.print_recs_based_on_genres()
-recently_played.print_recently_played()
+#genre_recs.print_recs_based_on_genres()
+#recently_played.print_recently_played(50)
+
+#print(json.dumps(playlists.get_current_user_playlists(defaults.SP_CLIENT, 5), indent=3))
+
+#print(json.dumps(playlists.get_current_user_playlists_to_track_ids(defaults.SP_CLIENT, 5, 5), indent=3))
+
+# Load playlist metadata first, then load it for all the tracks on it afterward
+#playlists_no_tracks = playlists.get_current_user_playlists_no_tracks(defaults.SP_CLIENT, 50)
+#print(json.dumps(playlists_no_tracks, indent=3))
+#playlists.print_playlists_rankings(defaults.TRAIT_TEMPO, True, 50, 50)
+
+#print(json.dumps(playlists.get_current_user_playlists_no_tracks(defaults.SP_CLIENT, 5), indent=2))
+#print(json.dumps(playlists.get_current_user_playlists_to_track_ids(defaults.SP_CLIENT, 50, 50), indent=3))
+#print(json.dumps(playlists.get_current_user_playlists_to_track_ids_with_audio_features(defaults.SP_CLIENT, 5, 5), indent=2))
+playlists.get_current_user_playlists_to_track_ids_with_audio_features(defaults.SP_CLIENT, 5, 5)
--- src/playlists.py
+++ src/playlists.py
@@ -1,6 +1,8 @@
 import defaults
+import json
 import tracks
 
+# Raw response from API - shared by preemptive load and lazy load methods
 def get_current_user_playlists(
     sp = defaults.SP_CLIENT,
     limit=5
@@ -9,6 +11,36 @@ def get_current_user_playlists(
         limit=limit,
         user=sp.current_user()['id']
     )['items']
+
+# Used for lazy loading the tracks in favor of showing all the playlist metadata first
+def get_current_user_playlists_no_tracks(
+    sp = defaults.SP_CLIENT,
+    playlist_limit=5
+):
+    playlists_info_no_tracks = {}
+    user_playlists = get_current_user_playlists(sp, playlist_limit)
+    for playlist in user_playlists:
+        pid=playlist['id']
+        playlists_info_no_tracks[pid] = {
+            'name': playlist['name'],
+            'description': playlist['description'],
+            'tracks': {}
+        }
+    return playlists_info_no_tracks
+
+# Used to get the tracks from a single playlist on demand - the graphing UI can cache it in memory if needed
+def get_tracks_from_playlist(
+    playlist_id,
+    sp = defaults.SP_CLIENT,
+    limit=defaults.TRACK_LIMIT
+):
+    tracks_from_this_playlist = sp.user_playlist_tracks(
+        user=sp.current_user()['id'],
+        playlist_id=playlist_id,
+        limit=limit
+    )['items']
+    track_ids = [t['track']['id'] for t in tracks_from_this_playlist]
+    return tracks.get_tracks_from_ids(sp, track_ids)
 
 def get_current_user_playlists_to_track_ids(
     sp = defaults.SP_CLIENT,
@@ -19,60 +51,91 @@ def get_current_user_playlists_to_track_ids(
     user_playlists = get_current_user_playlists(sp, playlist_limit)
     for playlist in user_playlists:
         pid=playlist['id']
-        tracks_from_this_playlist = sp.user_playlist_tracks(
-            user=sp.current_user()['id'],
-            playlist_id=pid,
-            limit=track_limit
-        )
         playlists_to_track_ids[pid] = {
             'name': playlist['name'],
             'description': playlist['description'],
             'tracks': {}
         }
-        track_ids = []
-        for list_track in tracks_from_this_playlist['items']:
-            tid = list_track['track']['id']
-            track_ids.append(tid)
-            playlists_to_track_ids[pid]['tracks'][tid] = {}
-        track_data = tracks.get_tracks_from_ids(sp, track_ids)
-        for tid in track_ids:
-            playlists_to_track_ids[pid]['tracks'][tid] = track_data[tid]
+        playlists_to_track_ids[pid]['tracks'] = get_tracks_from_playlist(pid, sp, track_limit)
+
     return playlists_to_track_ids
+
+def get_tracks_with_audio_features(
+    track_ids,
+    sp = defaults.SP_CLIENT
+):
+    tracks_with_audio_features = {}
+    tbf = tracks.get_tracks_by_features(sp, track_ids)
+    for track_id, features in tbf.items():
+        print({track_id})
+        print("FEATURES:", features)
+        tracks_with_audio_features[track_id] = {}
+        tracks_with_audio_features[track_id]['audio_features'] = features
+    return tracks_with_audio_features
+
+# Only really to be used after calling get_current_user_playlists_to_track_ids,
+# but can be used to load audio features lazily since it is one API call per track 
+# therefore by far the slowest call that be delayed until the playlist is selected
+def populate_audio_features_by_playlist(
+    playlist_id,
+    playlists_with_tracks = {},
+    sp = defaults.SP_CLIENT
+):
+    print(json.dumps(playlists_with_tracks[playlist_id]['tracks'], indent=2))
+    track_ids = playlists_with_tracks[playlist_id]['tracks'].keys()
+    tracks_with_audio_features = get_tracks_with_audio_features(track_ids, sp)
+    for tid in track_ids:
+        playlists_with_tracks[playlist_id]['tracks'][tid]['audio_features'] = tracks_with_audio_features[tid]['audio_features']
+    #print("\n\n\n", playlist_id, "\n\nplaylists with tracks with audio features:", json.dumps(playlists_with_tracks))
+    return playlists_with_tracks
 
 def get_current_user_playlists_to_track_ids_with_audio_features(
     sp = defaults.SP_CLIENT,
-    playlist_limit=5,
-    track_limit=5
+    playlist_limit = defaults.PLAYLIST_LIMIT,
+    track_limit = defaults.TRACK_LIMIT
 ):
-    playlists_to_track_ids = get_current_user_playlists_to_track_ids(sp, playlist_limit, track_limit)
-    for pid in playlists_to_track_ids:
-        pl_track_ids = playlists_to_track_ids[pid]['tracks'].keys()
-        tbf = tracks.get_tracks_by_features(sp, pl_track_ids)
-        for tid in playlists_to_track_ids[pid]['tracks']:
-            playlists_to_track_ids[pid]['tracks'][tid]['audio_features'] = tbf[tid]
-    return playlists_to_track_ids
+    playlists_with_tracks = get_current_user_playlists_to_track_ids(sp, playlist_limit, track_limit)
+    for pid in playlists_with_tracks:
+        playlists_with_tracks = populate_audio_features_by_playlist(pid, playlists_with_tracks)
+    return playlists_with_tracks
 
-## Prints first x sorted tracks by the selected audio feature for each of the first y user playlists found
-def print_playlists_rankings(
-    limit = defaults.TRACK_LIMIT,
+## Gets first x sorted tracks by the selected audio feature for each of the first y user playlists found
+def get_playlists_rankings(
     selected_feature_key = defaults.SELECTED_FEATURE_KEY,
-    descending_order = defaults.DESCENDING
+    descending_order = defaults.DESCENDING,
+    playlist_limit = defaults.PLAYLIST_LIMIT,
+    track_limit = defaults.TRACK_LIMIT
 ):
     playlists_with_ratings = get_current_user_playlists_to_track_ids_with_audio_features(
         defaults.SP_CLIENT,
-        defaults.PLAYLIST_LIMIT,
-        defaults.TRACK_LIMIT
+        playlist_limit,
+        track_limit
     )
+    sorted_playlists = {}
     for pid in playlists_with_ratings:
         playlist = playlists_with_ratings[pid]
-        new_sorted = tracks.get_sorted_tracks_by_feature(
+        sorted_playlists[pid] = {}
+        sorted_playlists[pid]['name'] = playlist['name']
+        sorted_playlists[pid]['description'] = playlist['description']
+        sorted_playlists[pid]['tracks'] = tracks.get_sorted_tracks_by_feature(
             playlist['tracks'],
             selected_feature_key,
             descending_order
         )
-        print("\n\nFor playlist \"" + playlist['name'] + "\"")
+    return sorted_playlists
+
+## Prints first x sorted tracks by the selected audio feature for each of the first y user playlists found
+def print_playlists_rankings(
+    selected_feature_key = defaults.SELECTED_FEATURE_KEY,
+    descending_order = defaults.DESCENDING,
+    playlist_limit = defaults.PLAYLIST_LIMIT,
+    track_limit = defaults.TRACK_LIMIT
+):
+    playlists_with_ratings = get_playlists_rankings(selected_feature_key, descending_order, playlist_limit, track_limit)
+    for pid in playlists_with_ratings:
+        print("\n\nFor playlist \"" + playlists_with_ratings[pid]['name'] + "\"")
         tracks.print_list_of_tracks_sorted_by_feature(
-            new_sorted,
+            playlists_with_ratings[pid]['tracks'],
             selected_feature_key,
             descending_order
         )
